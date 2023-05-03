b17756c8e6c6cbf5745a1df6b0f0dc2a314e840e
Yan Xu <yan.xu@sas.com>
2023-05-03 18:27:01 +0000

    Completed module

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,11 +1,13 @@
 # Runner script for all modules
-
+from load_data import load_sensor_data
 
 ##############################
 # Do not remove these two lines
 # They are needed to validate your unittest
-data = []
+data = load_sensor_data()
 print("Sensor Data App")
+print(f"Loaded records: {len(data)}")
+
 ##############################
 
 # Module 1 code here:
